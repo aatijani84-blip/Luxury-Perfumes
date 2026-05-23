c6d279a4a14a0a672eb666935a8262b50c7ade01
AHME AKEEM <aatijani84@gmail.com>
2026-05-23 05:11:24 +0000

    testing

--- src/Component/HomePage.jsx
+++ src/Component/HomePage.jsx
@@ -70,10 +70,9 @@ export function HomePage() {
                 <div className="flex items-center justify-center h-full w-full">
                     <Description />
                 </div>
-                <div className = 'shadow-lg rounded-lg w-full h-full overflow-hidden'>
-                    <div className="relative inset-0 aspect-video w-full h-full object-cover min-h-[400px] smh-[300px] transition-opacity duration-1000 block shrink-0 object-center saturate-100">
+                <div className='shadow-lg rounded-lg w-full h-full overflow-hidden'>
+                    <div className="relative inset-0 aspect-video w-full h-full object-cover min-h-75 sm:min-h-88 md:min-h-[400px] lg:min-h-[450px] transition-opacity duration-1000 shrink-0 object-center saturate-100">
                         <ImageSlider />
-                    <div className="grow"></div>
                     </div>
                 </div>
             </div>
